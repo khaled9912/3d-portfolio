dee997e2e174153d07c2af242997383c1bb379a6
username <email@email.com>
2024-05-22 22:48:50 +0200

    adding initial set up for the bird, plane, and rotation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,16 @@ import { Canvas } from '@react-three/fiber'
 import Loader from "../components/Loader";
 import Island from "../models/Island";
 import Sky from '../models/Sky';
+import Bird from '../models/Bird';
+import Plane from '../models/Plane';
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
 </div> */}
 const Home = () => {
+  const [isRotating, setIsRotating ] = useState(false);
+
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -25,19 +30,23 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <Canvas
-       className="w-full h-screen bg-transparent"
+       className= {`w-full h-screen bg-transparent ${isRotating? 'cursor-grabbing':'cursor-grab'}`}
        camera={{ near:0.1, far: 1000}}
        >
         <Suspense fallback={<Loader/>}>
           <directionalLight position={[1,1,1]} intensity={2}/>
           <ambientLight intensity={0.5}/>
           <hemisphereLight skyColor={"#b1e1ff"}  groundColor={"#000000"} intensity={1}/>
+          <Bird />
           <Sky />
           <Island 
           position = { islandPosition } 
           scale = { islandScale }
           rotation = { islandRotation }
+          isRotating = { isRotating}
+          setIsRotating = { setIsRotating }
           />
+          <Plane /> 
         </Suspense>
       </Canvas>
     </section>
