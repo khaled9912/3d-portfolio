1b327e37e64d7cca9c0324aef8aeefdb51fc24ad
username <email@email.com>
2024-05-25 18:45:29 +0200

    finalizing useFrame hook

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -34,32 +34,33 @@ const Island = ({ isRotating, setIsRotating, ...props}) =>  {
         lastX.current = clientX ;
 
     }
-    const handlePointerUp = (e) => {
+  const handlePointerUp = (e) => {
       e.stopProgation();  // it is going to prevent to touch any other func when you touch the screen unless this func.
       e.preventDefault(); // to prevent from reloading the page or doing anything like that.
       setIsRotating(false);
-      // when it touch we need to know where it is comming from a phone or tablet
-      const clientX = e.touches 
-        ? e.touches[0].clientX 
-        : e.clientX;
-        lastX.current = clientX ;
-        //calculate the change 
-        const delta = (clientX - lastX.current) / viewport.width;
-
-        islandRef.current.rotationy += delta * 0.01 * Math.PI;
-
-        lastX.current = clientX;
-        
+           
         
     }
-    const handlePointerMove = (e) => {
+  const handlePointerMove = (e) => {
       e.stopProgation();  // it is going to prevent to touch any other func when you touch the screen unless this func.
       e.preventDefault(); // to prevent from reloading the page or doing anything like that.
       setIsRotating(true);
 
-        if(isRotating) handlePointerUp(e) ; 
+        if(isRotating) {
+      // when it touch we need to know where it is comming from a phone or tablet
+      const clientX = e.touches 
+      ? e.touches[0].clientX 
+      : e.clientX;
+      lastX.current = clientX ;
+      //calculate the change 
+      const delta = (clientX - lastX.current) / viewport.width;
+
+      islandRef.current.rotationy += delta * 0.01 * Math.PI;
+
+      lastX.current = clientX;
+        } ; 
     }
-    const handleKeyDown = (e) => {
+  const handleKeyDown = (e) => {
       if(e.key == 'ArrowLeft') {
         if(!isRotating)  setIsRotating(true);
         islandRef.current.rotation.y += 0.01 * Math.PI;
@@ -69,25 +70,78 @@ const Island = ({ isRotating, setIsRotating, ...props}) =>  {
         }
     }
 
-    const  handleKeyUp =  (e) => {
+  const  handleKeyUp =  (e) => {
       if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
         setIsRotating(false);
       }
     }
 
+  useFrame( () => {
+      if(!isRotating) {
+        rotationSpeed.current *= dampingFactor;
+        if (Math.abs(rotationSpeed.current) < 0.001) {
+          rotationSpeed.current = 0;
+        }
+        islandRef.current.rotation.y += rotationSpeed.current;
+      }else{
+        const rotation = islandRef.current.rotation.y;
+      }
+
+      /**
+       * Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].
+       * The goal is to ensure that the rotation value remains within a specific range to
+       * prevent potential issues with very large or negative rotation values.
+       *  Here's a step-by-step explanation of what this code does:
+       *  1. rotation % (2 * Math.PI) calculates the remainder of the rotation value when divided
+       *     by 2 * Math.PI. This essentially wraps the rotation value around once it reaches a
+       *     full circle (360 degrees) so that it stays within the range of 0 to 2 * Math.PI.
+       *  2. (rotation % (2 * Math.PI)) + 2 * Math.PI adds 2 * Math.PI to the result from step 1.
+       *     This is done to ensure that the value remains positive and within the range of
+       *     0 to 2 * Math.PI even if it was negative after the modulo operation in step 1.
+       *  3. Finally, ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI) applies another
+       *     modulo operation to the value obtained in step 2. This step guarantees that the value
+       *     always stays within the range of 0 to 2 * Math.PI, which is equivalent to a full
+       *     circle in radians.
+       */
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    })
+
+      
+ 
 
     useEffect( () => {
-        document.addEventListener('pointerdown', handlePointerDown);
-        document.addEventListener('pointerup', handlePointerUp); 
-        document.addEventListener('pointermove', handlePointerMove); 
+      // we need to attach the below elements to canvas instead of the dom so if you touch the screen you are touching the canvas not the dom
+        const canvas = gl.domElement;
+        canvas.addEventListener('pointerdown', handlePointerDown);
+        canvas.addEventListener('pointerup', handlePointerUp); 
+        canvas.addEventListener('pointermove', handlePointerMove); 
         document.addEventListener('keydown', handleKeyDown); 
         document.addEventListener('keyup', handleKeyUp); 
 
 
         return () => {//removing events from the canvas once we cancel the page.
-          document.removeEventListener('pointerdown', handlePointerDown);
-          document.removeEventListener('pointerup', handlePointerUp); 
-          document.removeEventListener('pointerMove', handlePointerMove); 
+          canvas.removeEventListener('pointerdown', handlePointerDown);
+          canvas.removeEventListener('pointerup', handlePointerUp); 
+          canvas.removeEventListener('pointerMove', handlePointerMove); 
           document.removeEventListener('keydown', handleKeyDown); 
           document.removeEventListener('keyup', handleKeyUp); 
         }
